cc5413b82a0a0ed57365086392c64f81e4fbbd6f
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2022-12-06 23:38:18 +0000

    Added ability to move without pressing [ENTER] on Windows.

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,50 @@
 #include <iostream>
 
 // fmt Library
+// Equivalent to C++ 22's <format>, not supported in all compilers yet.
 #define FMT_HEADER_ONLY
 #include "../deps/fmt/format.h"
+
+// Include <conio.h> only on Windows (not supported elsewhere).
+// Provides support for entering commands without pressing [ENTER].
+#ifdef _WIN32
+    #include <conio.h>
+#endif
 
 using namespace std;
 
 int main() {
     char cmd;
     TwentyFourtyEight game;
-    cout << fmt::v9::format("{:^25}\n", 2048);
+    
+    // TODO: might move into a class function to make dynamic
+    // Welcome and Controls
+    cout << fmt::v9::format("{:=^25}\n", "Welcome to 2048");
+
+    #ifdef _WIN32
+        cout << "Since you are on Windows, move by pressing the move keys." << endl;
+        cout << "You do not have to press [ENTER] after each move key." << endl;
+    #else
+        cout << "Since you are not on Windows, move by pressing the move key and then [ENTER]." << endl;
+    #endif
+
+    cout << fmt::v9::format("{:=^25}\n", "Controls");
+    cout << "Use w for up, s for down, a for left, and d for right." << endl;
+    cout << "Use Q to quit the game early." << endl;
+    cout << fmt::v9::format("{:=^25}\n", "") << endl;
 
     while (true) {
         game.print_board();
 
-        cin >> cmd;
+        cout << "> ";
+
+        #ifdef _WIN32
+            cmd = getch();
+            cout << cmd << endl;
+        #else
+            cin >> cmd;
+        #endif
+
         switch (cmd) {
             case 'w':
                 game.move_up();
@@ -33,11 +63,11 @@ int main() {
                 game.move_right();
                 break;
             
-            case 'q':
+            case 'Q':
                 return 0;
             
             default:
-                cout << "Use the wasd keys to move. Use q to quit." << endl;
+                cout << "Use the wasd keys to move. Use Q to quit." << endl;
         }
 
         if (game.check_game_state()) {
@@ -45,5 +75,7 @@ int main() {
             cout << "Final Score: " << game.get_score() << endl;
             break;
         }
+
+        cout << fmt::v9::format("{:=^25}\n\n", "");
     }
 }
